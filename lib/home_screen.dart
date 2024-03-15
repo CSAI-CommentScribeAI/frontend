e0d0a33de3577f476998b8f4044b641264f6d435
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-03-15 23:45:07 +0900

    feat : 상단바에 컨테이너 겹치게 구현
    상단바 연장 박스, 알림 박스, 보유 가게 박스를 만든 후 보유 가게 박스는 offset을 이용하여 걸치게 구현

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:frontend/feedback/feedback_screen.dart';
 
@@ -7,48 +9,106 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(
-            180.0), // 상단바나 하단바의 높이를 변경하거나 다른 사용자 지정 위젯의 선호되는 크기를 지정
-        child: AppBar(
-          backgroundColor: const Color(0xFF374AA3),
-          title: const Padding(
-            padding: EdgeInsets.only(left: 10.0),
-            child: Text(
-              'CSAI 사장님',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
+      backgroundColor: const Color(0xFFF3F3FF),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF374AA3),
+        title: const Padding(
+          padding: EdgeInsets.only(left: 10.0),
+          child: Text(
+            'CSAI 사장님',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          centerTitle: false, // title 왼쪽 정렬
-          actions: [
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  // 뒤로가기 할 수 있게 페이지 이동
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const FeedbackPage(),
-                  ),
-                );
-              },
+        ),
+        centerTitle: false,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const FeedbackPage(),
+                ),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 10.0),
-                  child: Image.asset(
-                    'assets/images/feedback.png', // 이미지 경로 설정
-                    width: 28.24, // 이미지의 너비 설정
-                    height: 32, // 이미지의 높이 설정
-                    color: Colors.white, // 이미지의 색상 설정
-                  ),
+                padding: const EdgeInsets.only(right: 10.0),
+                child: Image.asset(
+                  'assets/images/feedback.png',
+                  width: 28.24,
+                  height: 32,
+                  color: Colors.white,
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 120,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF374AA3),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 19.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFD9D9D9),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        // Text의 폭을 무시한 채 화면의 가로폭에 맞추기 위해 사용
+                        child: Container(
+                          height: 40,
+                          alignment: Alignment.center,
+                          child: const Text(
+                            '알림 뜰 때만 보이게',
+                            textAlign: TextAlign.center, // 텍스트 중앙으로
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 19.0),
+
+                // Align 위젯으로 중앙 배치
+                child: Align(
+                  child: Transform.translate(
+                    offset: const Offset(0.0, 63.0), // X, Y 축으로 이동할 양을 지정
+                    child: Container(
+                      height: 144,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color(0xFF374AA3).withOpacity(0.5),
+                            blurRadius: 4,
+                            offset: const Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
