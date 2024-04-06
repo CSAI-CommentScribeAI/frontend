fd03ddcacf820e2020a15f95e4a0a01ceabfb993
thwlstn <thwlstn23@gmail.com>
2024-04-07 00:27:25 +0900

    이메일, 비밀번호 유효성 검사 구현
    email_validator 패키지 활용하여 _isEmailValid 설정 후 boolean 값으로 처리
    
    isPasswordValid 설정 후 boolean 값으로 처리

--- lib/Login_screen.dart
+++ lib/Login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -9,6 +10,16 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool isChecked = false;
+  bool _isEmailValid = true;
+  bool _isPasswordValid = true;
+  bool _isPasswordVisible = false;
+  late TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordController = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,13 +45,46 @@ class _LoginPageState extends State<LoginPage> {
                     enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(5),
                     ),
-                    labelText: '이메일',
+                    labelText: _isEmailValid ? '이메일' : '',
                     labelStyle: const TextStyle(fontSize: 13),
+                    errorText:
+                        _isEmailValid ? null : '올바른 이메일 형식이 아닙니다.',
                   ),
+                  onChanged: (value) {
+                    setState(() {
+                      if (value.isEmpty) {
+                        _isEmailValid = true;
+                      } else {
+                        _isEmailValid = EmailValidator.validate(value);
+                      }
+                    });
+                  },
                 ),
               ),
               const SizedBox(height: 12.0),
-              const PasswordTextField(),
+              PasswordTextField(
+                isPasswordValid: _isPasswordValid,
+                isPasswordVisible: _isPasswordVisible,
+                onChanged: (value) {
+                  setState(() {
+                    if (value.isEmpty ||
+                        value.length < 10 ||
+                        !RegExp(
+                                r'(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+])(?=.*[0-9]).{10,}')
+                            .hasMatch(value)) {
+                      _isPasswordValid = false;
+                    } else {
+                      _isPasswordValid = true;
+                    }
+                  });
+                },
+                onVisibilityToggle: () {
+                  setState(() {
+                    _isPasswordVisible = !_isPasswordVisible;
+                  });
+                },
+                controller: _passwordController,
+              ),
               const SizedBox(height: 1.0),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -48,11 +92,12 @@ class _LoginPageState extends State<LoginPage> {
                   Row(
                     children: [
                       Checkbox(
-                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-		                    visualDensity: const VisualDensity(
-			                  horizontal: VisualDensity.minimumDensity,
-			                  vertical: VisualDensity.minimumDensity,
-		                    ),
+                        materialTapTargetSize:
+                            MaterialTapTargetSize.shrinkWrap,
+                        visualDensity: const VisualDensity(
+                          horizontal: VisualDensity.minimumDensity,
+                          vertical: VisualDensity.minimumDensity,
+                        ),
                         value: isChecked,
                         onChanged: (bool? value) {
                           setState(() {
@@ -60,10 +105,8 @@ class _LoginPageState extends State<LoginPage> {
                           });
                         },
                       ),
-                      
                       TextButton(
-                        onPressed: () {
-                        },
+                        onPressed: () {},
                         style: TextButton.styleFrom(
                           padding: EdgeInsets.zero,
                         ),
@@ -81,9 +124,8 @@ class _LoginPageState extends State<LoginPage> {
                   Row(
                     children: [
                       TextButton(
-                        onPressed: () {
-                        },
-                        child:  Text(
+                        onPressed: () {},
+                        child: Text(
                           '아이디',
                           style: TextStyle(
                             color: Colors.black.withOpacity(0.5),
@@ -97,11 +139,11 @@ class _LoginPageState extends State<LoginPage> {
                         style: TextStyle(
                           color: Colors.black.withOpacity(0.5),
                           fontWeight: FontWeight.bold,
-                        ),),
-                       TextButton(
-                        onPressed: () {
-                        },
-                        child:  Text(
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
                           '비밀번호',
                           style: TextStyle(
                             color: Colors.black.withOpacity(0.5),
@@ -110,77 +152,81 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                         ),
                       ),
-                       Text(
+                      Text(
                         '찾기',
                         style: TextStyle(
-                           color: Colors.black.withOpacity(0.5),
-                           fontWeight: FontWeight.bold,
-                           fontSize: 12,
+                          color: Colors.black.withOpacity(0.5),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
                         ),
-                        ),
+                      ),
                     ],
                   ),
                 ],
               ),
-                     Padding(
-                       padding: const EdgeInsets.only(top: 17),
-                       child: SizedBox(
-                        height: 45,
-                        width: 400,
-                         child: TextButton(
-                          onPressed: () {
-                          },
-                           style: ButtonStyle(
-                           backgroundColor: MaterialStateProperty.all<Color>(const Color(0xff374AA3)),
-                           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                             borderRadius: BorderRadius.circular(5),
-                         ),
-                                            ),
-                                         ),
-                         child: const Text(
-                          'Login',
-                         style: TextStyle(
-                         color: Colors.white,
-                         fontWeight: FontWeight.bold,
-                         fontSize: 16,
-                         ),
-                        ),
-                       ),
-                       ),
-                     ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 17),
+              Padding(
+                padding: const EdgeInsets.only(top: 17),
+                child: SizedBox(
+                  height: 45,
+                  width: 400,
                   child: TextButton(
-                    onPressed: () {
-                    },
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(const Color(0xff374AA3)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
                     child: const Text(
-                      '사장님 앱이 처음이신가요?',
+                      'Login',
                       style: TextStyle(
+                        color: Colors.white,
                         fontWeight: FontWeight.bold,
                         fontSize: 16,
-                      
                       ),
                     ),
                   ),
-                )
-          ]),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 17),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    '사장님 앱이 처음이신가요?',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
+class PasswordTextField extends StatelessWidget {
+  final bool isPasswordValid;
+  final bool isPasswordVisible;
+  final ValueChanged<String> onChanged;
+  final VoidCallback? onVisibilityToggle;
+  final TextEditingController controller;
 
-class PasswordTextField extends StatefulWidget {
-  const PasswordTextField({super.key});
-
-  @override
-  _PasswordTextFieldState createState() => _PasswordTextFieldState();
-}
-
-class _PasswordTextFieldState extends State<PasswordTextField> {
-  bool _obscureText = true;
+  const PasswordTextField({
+    super.key,
+    required this.isPasswordValid,
+    required this.isPasswordVisible,
+    required this.onChanged,
+    this.onVisibilityToggle,
+    required this.controller,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -190,25 +236,28 @@ class _PasswordTextFieldState extends State<PasswordTextField> {
         alignment: Alignment.centerRight,
         children: [
           TextField(
-            obscureText: _obscureText,
+            controller: controller,
+            obscureText: !isPasswordVisible,
+            onChanged: onChanged,
             decoration: InputDecoration(
               enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(5),
               ),
-              labelText: '비밀번호',
+              labelText: isPasswordValid ? '비밀번호' : '',
               labelStyle: const TextStyle(fontSize: 13),
+              errorText: isPasswordValid
+                  ? null
+                  : '비밀번호는 적어도 하나의 영문자, 특수문자 포함 10자 이상이어야 합니다.',
             ),
           ),
+          
           Positioned(
             right: 0,
+            top: isPasswordValid ? 6 : -10,
             child: TextButton(
-              onPressed: () {
-                setState(() {
-                  _obscureText = !_obscureText;
-                });
-              },
+              onPressed: onVisibilityToggle,
               child: Text(
-                _obscureText ? '보기' : '숨기기',
+                isPasswordVisible ? '숨기기' : '보기',
                 style: TextStyle(
                   color: Colors.black.withOpacity(0.5),
                   fontWeight: FontWeight.bold,
@@ -221,4 +270,3 @@ class _PasswordTextFieldState extends State<PasswordTextField> {
     );
   }
 }
-
