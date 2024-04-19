128032c6c88e90a97eccee221371c17cc799dd0a
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-04-19 11:46:36 +0900

    refactor : 가게 정보 텍스트필드 따로 구현
    가게 정보 텍스트필드를 따로 빼서 파일을 만든 후 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/Login_screen.dart';
+import 'package:get/get.dart';
 
 void main() {
+  // GetX 서비스 초기화
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -11,7 +14,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    // snackbar를 사용하기 위해 GetX 컨텍스트 초기화를 위해 작성
+    return const GetMaterialApp(
       home: LoginPage(),
     );
   }
--- lib/screens/store_screen.dart
+++ lib/screens/store_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/widgets/storeForm_widget.dart';
 import 'package:get/get.dart';
 
 class StorePage extends StatefulWidget {
@@ -20,7 +21,7 @@ class _StorePageState extends State<StorePage> {
 
   String register = '110-89-341392';
   String name = 'BBQ 코엑스점';
-  String phoneNumber = '02-343-1230';
+  String phoneNumber = '343-1230';
   String address = '서울특별시 강남구 영동대로 513';
   String explanation = '가게 설명글';
   int minOrderPrice = 18000;
@@ -197,12 +198,14 @@ class _StorePageState extends State<StorePage> {
                         value: phoneNumber,
                         label: '음식점 전화번호',
                         selectedAreaCode: selectedAreaCode, // 선택된 지역번호
-                        onAreaCodeChanged: (newValue) {
-                          setState(() {
-                            selectedAreaCode = newValue ??
-                                selectedAreaCode; // 지역번호가 바뀌면 newValue값을 업데이트해 selectedAreaCode에 저장
-                          });
-                        },
+                        onAreaCodeChanged: saveColor // 편집 버튼 누를 때만 드롭다운 버튼 활성화
+                            ? (newValue) {
+                                setState(() {
+                                  selectedAreaCode = newValue ??
+                                      selectedAreaCode; // 지역번호가 바뀌면 newValue값을 업데이트해 selectedAreaCode에 저장
+                                });
+                              }
+                            : null,
                         showAreaCodeDropdown: true, // 지역번호 선택 드롭다운 표시 여부
                         editable: saveColor ? false : true,
                         onSaved: (val) {
@@ -494,76 +497,4 @@ class _StorePageState extends State<StorePage> {
       ),
     );
   }
-}
-
-// 텍스트필드 위젯 생성 함수
-storeTextFormField({
-  required dynamic value,
-  required String label,
-  required FormFieldSetter onSaved,
-  required FormFieldValidator validator,
-  required bool editable,
-  required String suffixText,
-  required bool showAreaCodeDropdown, // 새로운 인자: 지역번호 선택 드롭다운을 표시할지 여부
-  String? selectedAreaCode, // 새로운 인자: 선택된 지역번호, 필수가 아님
-  void Function(String?)?
-      onAreaCodeChanged, // 새로운 인자: 지역번호 변경 시 호출될 콜백 함수, 필수가 아님
-}) {
-  return Column(
-    children: [
-      Row(
-        children: [
-          Text(
-            label,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w700,
-              color: Color(0xFF7E7EB2),
-            ),
-          ),
-        ],
-      ),
-      Row(
-        children: [
-          // 지역번호 선택을 위한 드롭다운
-          showAreaCodeDropdown
-              ? DropdownButton<String>(
-                  value: selectedAreaCode, // 선택된 지역번호
-                  onChanged: onAreaCodeChanged, // 지역번호 변경 시 호출될 함수
-                  items: <String>['02', '031', '032', '033'] // 지역번호 목록
-                      .map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                )
-              : Container(width: 0),
-          const SizedBox(width: 8), // 조금의 간격을 추가하여 텍스트 필드와 분리
-          Expanded(
-            child: TextFormField(
-              initialValue: value.toString(),
-              onSaved: onSaved, // 폼 필드가 저장될 때 호출
-              validator: validator, // 입력된 값의 유효성을 검사
-              readOnly: editable, // 읽기/쓰기 권한
-              textAlign: TextAlign.center, // 중앙 정렬
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: const EdgeInsets.all(5.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                  borderSide: BorderSide.none,
-                ),
-                suffixText: suffixText,
-              ),
-            ),
-          ),
-        ],
-      ),
-      const SizedBox(
-        height: 18,
-      ),
-    ],
-  );
 }
