67581880cdd26e736eb85347b2ea26e821db2ad5
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-04-24 09:02:16 +0900

    feat : #17 팝업메뉴버튼 구현
    PopupMenuButton을 사용해 더보기 버튼 클릭 시 메뉴 아이템들 나오게 구현

--- lib/widgets/userReview_widget.dart
+++ lib/widgets/userReview_widget.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class UserReview extends StatelessWidget {
+class UserReview extends StatefulWidget {
   // ReviewPage에서 index로 reviewList의 하나의 객체마다 접근을 하고
   // Listview.builder에서 사용해야하기 때문에 객체 review를 받아야 함
   // 리스트 받으면 아래의 코드에서 리스트안의 객체를 받아야하는 코드를 구현해야 함
   final Map<String, dynamic> review;
   const UserReview({required this.review, super.key});
 
+  @override
+  State<UserReview> createState() => _UserReviewState();
+}
+
+enum MenuItem { menuItem1, menuItem2, menuItem3 }
+
+class _UserReviewState extends State<UserReview> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -17,13 +24,13 @@ class UserReview extends StatelessWidget {
           leading: ClipRRect(
             borderRadius: BorderRadius.circular(10.0),
             child: Image.asset(
-              review["profileImgPath"],
+              widget.review["profileImgPath"],
             ),
           ),
 
           // 이름
           title: Text(
-            review["name"],
+            widget.review["name"],
             style: const TextStyle(
               fontSize: 20,
             ),
@@ -32,10 +39,10 @@ class UserReview extends StatelessWidget {
           // 리뷰 등록일과 별점
           subtitle: Row(
             children: [
-              Text(review["open_date"]),
+              Text(widget.review["open_date"]),
               const SizedBox(width: 8.0),
               RatingBarIndicator(
-                rating: review["rate"],
+                rating: widget.review["rate"],
                 itemSize: 20.0,
                 itemBuilder: (context, index) {
                   return const Icon(
@@ -48,7 +55,20 @@ class UserReview extends StatelessWidget {
           ),
 
           // 더보기란(위치를 title과 같은 행으로 배치하기 위해 isThreeLine을 true로 설정)
-          trailing: const Icon(Icons.more_vert),
+          trailing: PopupMenuButton<MenuItem>(
+            color: const Color(0xFF7783C2),
+            itemBuilder: (BuildContext context) {
+              return [
+                menuItem('신고'),
+                const PopupMenuDivider(),
+                menuItem('차단'),
+                const PopupMenuDivider(),
+                menuItem('숨김'),
+              ];
+            },
+            child: const Icon(Icons.more_vert_rounded),
+          ),
+
           isThreeLine: true,
         ),
         const SizedBox(height: 5),
@@ -59,7 +79,7 @@ class UserReview extends StatelessWidget {
           title: Padding(
             padding: const EdgeInsets.only(bottom: 5.0),
             child: Text(
-              review["menu"],
+              widget.review["menu"],
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
@@ -70,7 +90,7 @@ class UserReview extends StatelessWidget {
 
           // 리뷰 댓글
           subtitle: Text(
-            review["review"],
+            widget.review["review"],
             style: TextStyle(
               color: const Color(0xFF000000).withOpacity(0.6),
             ),
@@ -93,7 +113,7 @@ class UserReview extends StatelessWidget {
             child: ClipRRect(
               borderRadius: BorderRadius.circular(10.0),
               child: Image.asset(
-                review["menuImgPath"],
+                widget.review["menuImgPath"],
                 width: 64,
                 height: 70,
                 fit: BoxFit.cover,
@@ -104,4 +124,23 @@ class UserReview extends StatelessWidget {
       ],
     );
   }
+}
+
+// 팝업메뉴
+PopupMenuItem<MenuItem> menuItem(String text) {
+  return PopupMenuItem<MenuItem>(
+    enabled: true, // 팝업메뉴 호출(ex: onTap()) 가능
+    onTap: () {},
+    value: MenuItem.menuItem1,
+    height: 30,
+    child: Center(
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  );
 }
