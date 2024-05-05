6fdfbf68f8e6b93d1b1fd34879d42c9184023e3f
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-05-06 08:47:04 +0900

    feat : #21 편지 서비스 페이지에 있는 버튼 구현 및 아이콘 리스트 이동 버튼 구현
    - 수정 버튼을 누를 경우 캐릭터와 글씨체 버튼 나오게 구현
    - 완료 버튼을 눌렀을 때 출력하기 버튼 나오게 구현(편지 서비스 들어갈 때 default UI)
    - 다음 혹은 이전 아이콘 리스트를 보여주기 위한 버튼 구현하고 그거에 맞게 draggble 코드 수정

--- lib/screens/letter_screen.dart
+++ lib/screens/letter_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:flutter_font_picker/flutter_font_picker.dart';
 
 class LetterPage extends StatefulWidget {
   const LetterPage({super.key});
@@ -10,30 +10,21 @@ class LetterPage extends StatefulWidget {
 }
 
 class _LetterPageState extends State<LetterPage> {
-  bool isBottomShowed = false; // 아이콘 하단바 보여주기
+  late ScrollController scrollController;
+  bool charactersOpened = false;
+  bool isCompleted = true; // 수정 완료 활성화 여부
   CarouselController controller = CarouselController();
-  int currentPageIndex = 0; // 중앙 페이지의 인덱스를 추적하기 위한 변수
-  bool isCurrent = false;
-
+  int currentPageIndex = 0;
   List<Widget> overlayWidgets = [];
 
+  int startIndex = 0;
+  int endIndex = 7;
+
   List<Map<String, dynamic>> templateList = [
-    {
-      'title': '크리스마스',
-      'url': 'assets/templates/christmas.png',
-    },
-    {
-      'title': '나뭇잎',
-      'url': 'assets/templates/leaf.png',
-    },
-    {
-      'title': '가을바람',
-      'url': 'assets/templates/autumn.png',
-    },
-    {
-      'title': '개구리',
-      'url': 'assets/templates/frog.png',
-    },
+    {'title': '크리스마스', 'url': 'assets/templates/christmas.png'},
+    {'title': '나뭇잎', 'url': 'assets/templates/leaf.png'},
+    {'title': '가을바람', 'url': 'assets/templates/autumn.png'},
+    {'title': '개구리', 'url': 'assets/templates/frog.png'},
   ];
 
   List<Map<String, dynamic>> iconList = [
@@ -48,7 +39,51 @@ class _LetterPageState extends State<LetterPage> {
     {'url': 'assets/characters/santaClausBag.png'},
     {'url': 'assets/characters/snowflake.png'},
     {'url': 'assets/characters/sun.png'},
+    {'url': 'assets/characters/chick.png'},
+    {'url': 'assets/characters/flower.png'},
+    {'url': 'assets/characters/smile.png'},
   ];
+
+  List<Map<String, dynamic>> nextIconList = [];
+
+  void loadNextIcons() {
+    setState(() {
+      // index 7번 뒤로 이동
+      startIndex += 7;
+      endIndex += 7;
+      if (endIndex > iconList.length) {
+        // endIndex가 iconList의 길이를 초과하면
+        endIndex = iconList.length; // endIndex를 iconList의 길이로 설정하여 초과하지 않도록 보정
+        startIndex = endIndex - 7; // startIndex도 맞춰서 수정
+      }
+    });
+  }
+
+  // 이전 버튼 콜백 함수
+  void loadPreviousIcons() {
+    setState(() {
+      // 7개를 보여주기 때문에 index 7번 앞으로 이동
+      startIndex -= 7;
+      endIndex -= 7;
+      if (startIndex < 0) {
+        // startIndex가 음수가 되면
+        startIndex = 0; // startIndex를 0으로 설정하여 음수가 되지 않도록 보정
+        endIndex = startIndex + 7; // endIndex도 맞춰서 수정
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,139 +112,127 @@ class _LetterPageState extends State<LetterPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // 편지내역 버튼
             Padding(
               padding: const EdgeInsets.only(top: 10.0, left: 300.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF7B88C2),
-                  elevation: 3.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                  minimumSize: const Size(80, 35),
-                ),
-                child: const Text(
-                  '편지 내역',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
+
+              // 수정, 완료 버튼
+              child: isCompleted
+                  ? ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          isCompleted = !isCompleted;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF7B88C2),
+                        elevation: 3.0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        minimumSize: const Size(50, 30),
+                      ),
+                      child: const Text(
+                        '수정',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  : ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          isCompleted = !isCompleted;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF7B88C2),
+                        elevation: 3.0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        minimumSize: const Size(50, 30),
+                      ),
+                      child: const Text(
+                        '완료',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
             ),
             const SizedBox(height: 12),
 
-            // 템플릿 리스트
-            Expanded(
+            // 편지 템플릿
+            Flexible(
               child: makeList(),
             ),
 
-            // 캐릭터 추가 버튼
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  isBottomShowed = !isBottomShowed;
-                });
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF7B88C2),
-                elevation: 3.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                minimumSize: const Size(205, 38),
-              ),
-              child: const Text(
-                '캐릭터 추가하기',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
+            isCompleted
+                ? const SizedBox(height: 0)
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      // 캐릭터, 글씨체 버튼
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            charactersOpened = !charactersOpened;
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: charactersOpened
+                              ? const Color(0xFF7B88C2)
+                              : const Color(0xFFD9D9D9),
+                          elevation: 3.0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          minimumSize: const Size(120, 40),
+                        ),
+                        child: const Text(
+                          '캐릭터',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            charactersOpened = !charactersOpened;
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFD9D9D9),
+                          elevation: 3.0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          minimumSize: const Size(120, 40),
+                        ),
+                        child: const Text(
+                          '글씨체',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
             const SizedBox(height: 15),
           ],
         ),
       ),
-      bottomNavigationBar: isBottomShowed
-          ? BottomAppBar(
-              color: const Color(0xFFE8E8FF),
-              height: 130,
-              child: Column(
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        '캐릭터 추가',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.add_circle),
-                      )
-                    ],
-                  ),
-                  Stack(
-                    children: [
-                      SingleChildScrollView(
-                        child: SizedBox(
-                          height: 55,
-                          child: GridView.builder(
-                            scrollDirection: Axis.horizontal,
-                            shrinkWrap: true,
-                            physics: const ScrollPhysics(),
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 1,
-                            ),
-                            itemCount: iconList.length,
-                            itemBuilder: (context, index) {
-                              return Draggable<Map<String, dynamic>>(
-                                data: iconList[index],
-                                feedback: Material(
-                                  elevation: 4.0,
-                                  child: Image.asset(
-                                    iconList[index]['url'],
-                                    height: 100,
-                                  ),
-                                ),
-                                child: Image.asset(
-                                  iconList[index]['url'],
-                                  height: 100,
-                                ),
-                                onDragEnd: (dragDetails) {
-                                  setState(() {
-                                    overlayWidgets.add(
-                                      Positioned(
-                                        left: dragDetails.offset.dx - 73,
-                                        top: dragDetails.offset.dy - 255,
-                                        child: Image.asset(
-                                          iconList[index]['url'],
-                                          height: 100,
-                                        ),
-                                      ),
-                                    );
-                                  });
-                                },
-                              );
-                            },
-                          ),
-                        ),
-                      ),
-                      ...overlayWidgets,
-                    ],
-                  ),
-                ],
-              ),
-            )
-          : ElevatedButton(
+
+      // 하단바
+      bottomNavigationBar: isCompleted
+          ? ElevatedButton(
               onPressed: () {},
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFF374AA3),
@@ -222,25 +245,134 @@ class _LetterPageState extends State<LetterPage> {
                 ),
               ),
               child: const Text(
-                '템플릿 저장',
+                '출력하기',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
+            )
+          : charactersOpened
+              ? BottomAppBar(
+                  color: const Color(0xFFE8E8FF),
+                  height: 130,
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // 캐릭터 추가 버튼
+                          Row(
+                            children: [
+                              const Text(
+                                '캐릭터 추가',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.add_circle),
+                              ),
+                            ],
+                          ),
+
+                          // 캐릭터 이동 버튼
+                          Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  loadPreviousIcons();
+                                },
+                                icon: const Icon(Icons.arrow_back_ios),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  loadNextIcons();
+                                },
+                                icon: const Icon(Icons.arrow_forward_ios),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Stack(
+                        children: [
+                          SizedBox(
+                            height: 55,
+                            child: Row(
+                              children: iconList
+                                  // iconList의 startIndex부터 endIndex까지의 아이콘 이미지를 가져옴
+                                  .sublist(startIndex, endIndex)
+                                  .map(
+                                    (icon) => Draggable<Map<String, dynamic>>(
+                                      // Draggable 위젯에서 가져올 아이콘 지정
+                                      data: icon,
+
+                                      // 드래그 동안 화면에 나타날 아이콘 지정
+                                      feedback: Image.asset(
+                                        icon['url'],
+                                        height: 53,
+                                        width: 53,
+                                      ),
+
+                                      // 캐릭터 박스에 있는 아이콘 지정
+                                      child: Image.asset(
+                                        icon['url'],
+                                        height: 53,
+                                        width: 53,
+                                      ),
+
+                                      // 드래그가 완료될 때의 콜백 함수
+                                      // 오버레이 위젯에 추가될 위치를 계산하고, 그 위치에 이미지 위젯을 추가
+                                      onDragEnd: (details) {
+                                        setState(() {
+                                          RenderBox overlay = Overlay.of(
+                                                  context) // 다른 위젯 위에 겹쳐질 위젯 구현
+                                              .context
+                                              .findRenderObject() as RenderBox;
+                                          final relative =
+                                              overlay.globalToLocal(details
+                                                  .offset); // 드래그한 위치를 상대적인 좌표로 변환
+                                          overlayWidgets.add(
+                                            Positioned(
+                                              // 드롭할 위치 지정
+                                              left: relative.dx - 55,
+                                              top: relative.dy - 230,
+                                              child: Image.asset(
+                                                icon['url'],
+                                                height: 53,
+                                                width: 53,
+                                              ),
+                                            ),
+                                          );
+                                        });
+                                      },
+                                    ),
+                                  )
+                                  .toList(),
+                            ),
+                          ),
+                          ...overlayWidgets,
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              : null,
     );
   }
 
-  // 편지지 템플릿 Carousel 리스트
   CarouselSlider makeList() {
     return CarouselSlider.builder(
       carouselController: controller,
       itemCount: templateList.length,
       itemBuilder: (context, index, pageViewIndex) {
         return DragTarget<String>(
-          onWillAccept: (data) => true, // 모든 데이터를 받아들임
+          onWillAccept: (data) => true,
           onAccept: (data) {},
           builder: (context, candidateData, rejectedData) {
             return Column(
@@ -286,7 +418,6 @@ class _LetterPageState extends State<LetterPage> {
         viewportFraction: 1,
         aspectRatio: 1 / 1.4,
         scrollDirection: Axis.horizontal,
-        // 페이지를 넘길 때마다 중앙 페이지 인덱스를 알기 위해 currentCenterPageIndex에 저장
         onPageChanged: (index, reason) {
           setState(() {
             currentPageIndex = index;
