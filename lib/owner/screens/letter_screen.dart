adca04bf7b852439ff8b73952cd1506657ebb53c
thwlstn <thwlstn23@gmail.com>
2024-06-08 02:19:21 +0900

    feat(owner) : #91 편지 저장 & 주문 수락 후 편지 생성 & 배달 완료 API 연동
    - 출력하기 버튼을 클릭했을 때 orderId 값에 따라 DeliveryService().completeDelivery() 객체방식으로 매서드에 접근하여 적용

--- lib/owner/screens/letter_screen.dart
+++ lib/owner/screens/letter_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:frontend/owner/services/letter_service.dart';
+import 'package:frontend/owner/services/delivery_service.dart';
 
 class LetterPage extends StatefulWidget {
   const LetterPage({super.key});
@@ -436,6 +437,7 @@ class _LetterPageState extends State<LetterPage> {
           ? ElevatedButton(
               onPressed: () {
                 Navigator.pop(context);
+                DeliveryService().completeDelivery(4);
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFF374AA3),
---
+++ lib/owner/services/delivery_service.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+import 'dart:io' show Platform;
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DeliveryService {
+  late String serverAddress;
+
+  // 배달 완료 API
+  Future<List<Map<String, dynamic>>> completeDelivery(int orderId) async {
+    // SharedPreferences에서 액세스 토큰을 가져옴
+    final prefs = await SharedPreferences.getInstance();
+    final accessToken = prefs.getString('accessToken') ?? '';
+
+    // 편지 인스턴스를 저장할 리스트를 생성
+    List<Map<String, dynamic>> completeDeliveryInstance = [];
+
+    // 현재 플랫폼에 따라 서버 주소 설정
+    if (Platform.isAndroid) {
+      serverAddress =
+          'http://10.0.2.2:9000/api/v1/cart/orders/delivery/$orderId';
+    } else if (Platform.isIOS) {
+      serverAddress =
+          'http://127.0.0.1:9000/api/v1/cart/orders/delivery/$orderId';
+    }
+
+    try {
+      // 서버에 요청 보냄
+      final url = Uri.parse(serverAddress);
+
+      // 헤더에 액세스 토큰 추가
+      final headers = {
+        'Authorization': 'Bearer $accessToken',
+        'Content-Type': 'application/json',
+      };
+
+      // PUT 요청
+      final response = await http.put(
+        url,
+        headers: headers,
+      );
+
+      // 200 : 요청 성공
+      if (response.statusCode == 200) {
+        // 서버 응답을 UTF-8로 디코딩
+        final utf8Response = utf8.decode(response.bodyBytes);
+        // 디코딩된 응답을 JSON 형태로 변환
+        final dynamic jsonResponse = jsonDecode(utf8Response);
+
+        // jsonResponse가 Map이고 orderMenus 필드가 List일 경우
+        if (jsonResponse is Map && jsonResponse['orderMenus'] is List) {
+          final List<dynamic> deliveries = jsonResponse['orderMenus'];
+          print('JSON 데이터: $deliveries');
+
+          for (var delivery in deliveries) {
+            if (delivery is Map<String, dynamic>) {
+              completeDeliveryInstance.add(delivery);
+            }
+          }
+
+          print('배달 성공 $completeDeliveryInstance');
+          Get.snackbar("성공", "배달이 완료되었습니다.", backgroundColor: Colors.white);
+          return completeDeliveryInstance;
+        } else {
+          // 응답이 예상과 다를 경우
+          print('응답이 예상과 다름: $jsonResponse');
+          return [];
+        }
+      } else {
+        // 요청 실패 시
+        print('배달 실패: ${response.statusCode}');
+        print('응답 본문: ${response.body}');
+        Get.snackbar(
+          "실패",
+          "배달이 실패하였습니다.",
+          backgroundColor: Colors.white,
+          colorText: Colors.black,
+        );
+        return [];
+      }
+    } catch (e) {
+      // 예외 발생 시
+      print('예외 발생: $e');
+      Get.snackbar("오류", "예외가 발생하였습니다: $e", backgroundColor: Colors.white);
+      return [];
+    }
+  }
+}
