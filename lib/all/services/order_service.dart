01d4887a84f55741d871b0dbabef1b3dbf77928c
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-06-08 18:04:19 +0900

    feat(owner) : #95 주문 정보 api를 통해 구현
    - // await getStoreId() 작성할 때 FutureBuilder안에 await 사용할 수 없기 때문에
    - FutureBuilder의 future를 반환하는 함수 생성

--- lib/all/services/order_service.dart
+++ lib/all/services/order_service.dart
@@ -101,4 +101,50 @@ class OrderService {
       return [];
     }
   }
+
+  // 사장님 주문 조회 API
+  Future<List<OrderModel>> getUserOrders(int storeId) async {
+    final prefs = await SharedPreferences.getInstance();
+    final accessToken = prefs.getString('accessToken') ?? '';
+
+    List<OrderModel> userOrdersInstance = [];
+
+    if (Platform.isAndroid) {
+      serverAddress = 'http://10.0.2.2:9000/api/v1/cart/orders/$storeId';
+    } else if (Platform.isIOS) {
+      serverAddress = 'http://127.0.0.1:9000/api/v1/cart/orders/$storeId';
+    }
+    try {
+      final url = Uri.parse(serverAddress);
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Bearer $accessToken',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final utf8Response = utf8.decode(response.bodyBytes);
+        final dynamic jsonResponse = jsonDecode(utf8Response);
+
+        final List<dynamic> orders = jsonResponse;
+        print('주문 JSON 데이터: $orders');
+
+        for (var order in orders) {
+          userOrdersInstance.add(OrderModel.fromJson(order));
+        }
+
+        print('주문 조회 성공: $userOrdersInstance');
+        return userOrdersInstance;
+      } else {
+        print('주문 조회 실패');
+        return [];
+      }
+    } catch (e) {
+      print(
+        e.toString(),
+      );
+      return [];
+    }
+  }
 }
